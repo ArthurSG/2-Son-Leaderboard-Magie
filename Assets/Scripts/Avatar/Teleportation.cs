bd44e3315c6eb1e0be89aa9545cd3f580886f37c
Pierre <pierre.rellic@gmail.com>
2019-09-03 15:30:47 +0200

    Téléportation WIP

---
+++ Assets/Scripts/Avatar/Teleportation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour {
+  // Start is called before the first frame update
+  private Vector3 positionSaved;
+
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void Teleport() {
+    positionSaved = transform.position;
+
+    // lancer anim
+
+
+    // tp 
+    // lancer timer
+  }
+
+  public void Rappel(){
+    // anim de rappel
+    if (positionSaved != null){
+      transform.position = positionSaved;
+    }
+  }
+}
