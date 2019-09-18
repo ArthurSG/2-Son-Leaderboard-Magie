497a275e6f0f5f227848d5a732bde1ddd390a372
Arthur <Sorignet.gautrot.arthur@gmail.com>
2019-09-18 11:10:05 +0200

    Link Effect v1 + Avatar FX Managger + Rename Player GO as "Avatar"

---
+++ Assets/Scripts/Avatar/AvatarEffectsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarEffectsManager : MonoBehaviour{
+  
+  public List<ParticleSystem> Effects;
+
+  void Start(){
+    FetchComponents();
+        
+  }
+
+  void Update(){
+        
+  }
+
+  void AllerEffectsPlay(){
+  	foreach (ParticleSystem ps in Effects){
+  		ps.Play();
+  	}
+  }
+
+  void RetourEffectsStop(){
+  	foreach (ParticleSystem ps in Effects){
+  		ps.Stop();
+  	}
+  }
+
+
+
+  void FetchComponents(){
+
+  }
+}
---
+++ Assets/Scripts/Effects/LinkEffect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkEffect : MonoBehaviour{
+
+	private ParticleSystem particleSystem;
+	public GameObject spirit;
+
+	private float magnitude;
+	private Vector3 vecteurDistance;
+
+  void Start(){
+    FetchComponents ();
+  }
+
+  void Update(){
+  	this.transform.LookAt(spirit.transform);
+
+
+  	print (magnitude);
+  	DistanceWithTarget ();
+  	LifetimeModifier();
+  }
+
+  void DistanceWithTarget (){
+  	vecteurDistance = this.transform.position - spirit.transform.position;
+  	magnitude = vecteurDistance.magnitude;
+  }
+
+
+  void LifetimeModifier (){
+  	particleSystem.startLifetime = magnitude * 0.05f;
+  }
+
+
+
+  void FetchComponents(){
+  	particleSystem = GetComponent<ParticleSystem>();
+  }
+}
