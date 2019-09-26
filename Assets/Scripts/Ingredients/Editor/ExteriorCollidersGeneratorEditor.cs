5a48e84f0d5fa07b742257ee6eadd1bdf9abdeac
Skaje <sorignet.gautrot.arthur@gmail.com>
2019-09-26 13:06:36 +0200

    Borders generation

---
+++ Assets/Scripts/Ingredients/Editor/ExteriorCollidersGeneratorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ExteriorCollidersGenerator))]
+public class ExteriorCollidersGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ExteriorCollidersGenerator myScript = (ExteriorCollidersGenerator)target;
+        if (GUILayout.Button("Build Colliders"))
+        {
+            myScript.GenerateBorders();
+        }
+    }
+}
