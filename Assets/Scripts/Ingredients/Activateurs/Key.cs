edb38b8ef73aa50dc84a990f0dfe3cc6a177a51d
Pierre <pierre.rellic@gmail.com>
2019-09-30 15:14:30 +0200

    Clés préfab
    fonctionnel

---
+++ Assets/Scripts/Ingredients/Activateurs/Key.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+  private void OnTriggerEnter2D(Collider2D collision) {
+    print("oui");
+    if(collision.gameObject.tag == "Player"){
+      GameManager.instance.avatar.AddKey();
+      Destroy(this.gameObject);
+    }
+  }
+}
