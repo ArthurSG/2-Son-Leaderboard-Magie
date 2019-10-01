e33deeb0b7bbb220fcccf83b27690ab5e16d4102
Pierre <pierre.rellic@gmail.com>
2019-10-01 11:19:41 +0200

    Livre + HUD du livre

---
+++ Assets/Scripts/Ingredients/Zones/Livre.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Livre : MonoBehaviour
+{
+  public MenuManager manager;
+  public MenuPage page;
+
+  [SerializeField]
+  private string ID;
+
+  [SerializeField] [TextArea(3, 5)]
+  private string Content;
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    page.transform.GetChild(0).GetComponent<Text>().text = ID;
+    page.transform.GetChild(1).GetComponent<Text>().text = Content;
+
+    manager.SwapUIMenu(page);
+  }
+
+  private void OnTriggerExit2D (Collider2D collision) {
+    manager.Return();
+  }
+
+}
