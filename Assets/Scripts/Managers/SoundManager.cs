d3721d01340385161e60905b8a83e811cc1d18e8
Skaje <sorignet.gautrot.arthur@gmail.com>
2019-10-01 13:57:15 +0200

    Random small notes, sometimes

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,33 +4,44 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour{
 
-	private AudioClip[] musics = new AudioClip[2];
+	private AudioClip[] musics = new AudioClip[5];
 	private AudioClip music1;
 	private AudioClip music2;
 
-	private int randomMusicInt;
+	private int randomMusic;
+	private int randomTimeBetweenSongs;
 
 	private AudioSource source1;
 
   void Start(){
 
   	FetchComponents();
-
-  	
-  		randomMusicInt = Random.Range(0,2);
-
-  		print (randomMusicInt);
-
-  		source1.clip = musics[randomMusicInt];
-  		source1.Play(0);
-  	
-
-        
+  	 
   } 
+
+  void SongPlayer (){
+
+  	randomTimeBetweenSongs = Random.Range(240,10000);
+  	randomMusic = Random.Range(0,5);
+
+  	source1.clip = musics[randomMusic];
+  	source1.Play((ulong)randomTimeBetweenSongs);
+
+  	Timer SongPlay = new Timer (randomTimeBetweenSongs, SongPlayer);
+  	SongPlay.Play();
+
+
+  }
+
+
 
   void FetchComponents(){
   	source1 = GetComponent<AudioSource>();
   	musics[0] = Resources.Load<AudioClip>("Sounds/Music1");
   	musics[1]  = Resources.Load<AudioClip>("Sounds/Music2");
+  	musics[2] = Resources.Load<AudioClip>("Sounds/Music3");
+  	musics[3]  = Resources.Load<AudioClip>("Sounds/Music4");
+  	musics[4] = Resources.Load<AudioClip>("Sounds/Music5");
+
   }
 }
