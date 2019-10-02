99efff39aeb95ec9d6362ffa5064ee0a83565993
Bastien B <bas2205@live.fr>
2019-10-02 15:31:49 +0200

    Lamp and light halo added

---
+++ Assets/Scripts/Animation/StartAnimationAtRandomFrame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimationAtRandomFrame : MonoBehaviour {
+  public string animationName = "Idle";
+  private Animator animator;
+
+  void Start() {
+    animator = GetComponent<Animator>();
+    float randomTime = Random.Range(0f,1f);
+    animator.Play(animationName, 0, randomTime);
+  }
+
+}
