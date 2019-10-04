c75993ce02c452332ce68d54f94f9cef9ee6ad54
Pierre <pierre.rellic@gmail.com>
2019-10-04 14:47:40 +0200

    Portes logiques

--- Assets/Scripts/Ingredients/Activables/ActivationReceiver.cs
+++ Assets/Scripts/Ingredients/Activables/ActivationReceiver.cs
@@ -2,37 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActivationReceiver : Activable
-{
-    public MonoBehaviour[] linkedScripts;
+public class ActivationReceiver : Activable {
+  public MonoBehaviour[] linkedScripts;
 
-    protected override void ChangeState(bool state)
-    {
-        base.ChangeState(state);
+  protected override void ChangeState(bool state) {
+    base.ChangeState(state);
 
-        if (state)
-        {
-            foreach (MonoBehaviour script in linkedScripts)
-            {
-                script.enabled = true;
-            }
+    if (state) {
+      foreach (MonoBehaviour script in linkedScripts) {
+        script.enabled = true;
+      }
 
-        }
-
-        else
-        {
-            foreach (MonoBehaviour script in linkedScripts)
-            {
-                script.enabled = false;
-            }
-
-        }
-
-        SpriteChange(state);
     }
 
-    private void SpriteChange(bool state)
-    {
-        //TODO: Put Activated sprite or deactivated sprite
+    else {
+      foreach (MonoBehaviour script in linkedScripts) {
+        script.enabled = false;
+      }
+
     }
+
+    SpriteChange(state);
+  }
+
+  private void SpriteChange(bool state) {
+    //TODO: Put Activated sprite or deactivated sprite
+  }
 }
---
+++ Assets/Scripts/Ingredients/Activables/PorteLogique.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorteLogique : Activable {
+
+  private void Start() {
+    base.Start();
+
+  }
+  protected override void SetEtat() {
+    if (NombreSourcesRequise > 0) {
+      GetComponent<Activateur>().Desactiver();
+    }
+    GetComponent<Activateur>().Activer();
+  }
+}
