7e6e387bf0880576002f41d0c53c296ca9f8e884
Bastien B <bas2205@live.fr>
2019-10-04 16:27:13 +0200

    Layers above ground with script

---
+++ Assets/Scripts/Misc/ZIndexByPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZIndexByPosition : MonoBehaviour {
+  public SpriteRenderer spriteRenderer;
+  public bool updated = false;
+  private int distancePrecision = 10;
+  void Start() {
+    spriteRenderer.sortingOrder = (int) (-1 * transform.position.y * distancePrecision); 
+  }
+
+  void Update() {
+    if (updated)
+      spriteRenderer.sortingOrder = (int) (-1 * transform.position.y * distancePrecision);
+  }
+  
+}
